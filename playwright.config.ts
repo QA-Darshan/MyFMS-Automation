0b8508851bba87c3a34bd80c703c30a6c5e0570a
Darshan Prajapati <dprajapati@dc.nexuslink.com>
2026-04-27 17:36:19 +0530

    updated config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+ reporter: [
+  ['html'],                // ✅ keep HTML report
+  ['line'],                // console output
+  ['allure-playwright']    // Allure integration
+],
   timeout: 600000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
