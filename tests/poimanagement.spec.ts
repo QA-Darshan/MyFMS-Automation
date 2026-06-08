e6a3734c244c19d2b1e6685ee54f226ce5ecd1b8
Darshan Prajapati <dprajapati@dc.nexuslink.com>
2026-06-08 16:36:37 +0530

    Created different testcases and updated polygon issue

--- tests/poimanagement.spec.ts
+++ tests/poimanagement.spec.ts
@@ -7,33 +7,35 @@ function formatPolygon(coords: { lat: number; lng: number }[]) {
   const closed = [...coords, coords[0]];
 
   return closed
-    .map(c => `${c.lat};${c.lng}`)
+    .map(c => `${c.lat},${c.lng}`)  
     .join('|');
 }
 
-test('POI Management', async ({ page }) => {
+  const Addpoigroup = generatePOIGroupData();
+  const Updategroup = generatePOIGroupData();
+  const addpoi = generatePOIData();
+  const Updatepoi = generatePOIData();
+
+test.describe.serial('@regression POI Management', () => {
 
   const coords = [
     { lat: 52.67, lng: 6.46 },
     { lat: 52.64, lng: 6.60 },
     { lat: 52.59, lng: 6.49 }
   ];
-
-  const formatted = formatCoords(coords);
-
-  const Addpoigroup = generatePOIGroupData();
-  const Updategroup = generatePOIGroupData();
-  const addpoi = generatePOIData();
-  const Updatepoi = generatePOIData();
+  const formatted = formatCoords(coords, 'Dutch');
+  
+  test.beforeEach(async ({ page }) => {
   await login(page);
   await page.waitForTimeout(8000);
   await expect(page.getByTestId('sidebar-parent-menu-admin')).toBeVisible();
   await page.getByTestId('sidebar-parent-menu-admin').click();
 
   await page.getByTestId('sidebar-admin-child-menu-pois').click();
+})
 
+test('Add POI Group', async ({ page }) => {
   await page.getByTestId('poi-group-list-add-button-1').click();
-
   await page.getByTestId('poi-group-create-title-input').click();
   await page.getByTestId('poi-group-create-title-input').fill(Addpoigroup.title);
   await page.getByTestId('poi-group-create-description-textarea').click();
@@ -51,10 +53,9 @@ test('POI Management', async ({ page }) => {
 
   const AddPOIGroup = page.locator('[role="alert"], .alert, .rz-notification').first(); // ensure single element
   const AddPOIGroupmessage = (await AddPOIGroup.textContent())?.replace(/\s+/g, ' ').trim();
+})
 
-
-  //Edit POI Group
-
+  test('Edit POI Group', async ({ page }) => {
   const group = page.getByRole('option', { name: Addpoigroup.title });
   await expect(group).toBeVisible();
   await group.locator('button').click();
@@ -75,8 +76,10 @@ test('POI Management', async ({ page }) => {
 
   const EditPOIGroup = page.locator('[role="alert"], .alert, .rz-notification').first(); // ensure single element
   const EditPOIGroupmessage = (await EditPOIGroup.textContent())?.replace(/\s+/g, ' ').trim();
+  })
+  
 
-  //Add POI
+  test('Add POI', async ({ page }) => {
   await page.getByText(Updategroup.title).click();
 
   await page.getByTestId('poi-header-add-button').click();
@@ -94,8 +97,9 @@ test('POI Management', async ({ page }) => {
 
   const AddPOI = page.locator('[role="alert"], .alert, .rz-notification').first(); // ensure single element
   const AddPOImessage = (await AddPOI.textContent())?.replace(/\s+/g, ' ').trim();
-
-  // Edit POI
+  })
+  
+  test('Edit POI', async ({ page }) => {
   await page.getByTestId('poi-header-search-input').click();
   await page.getByTestId('poi-header-search-input').fill(addpoi.name);
 
@@ -114,7 +118,6 @@ test('POI Management', async ({ page }) => {
   await polygonRadio.click();
   await expect(polygonRadio).toHaveAttribute('aria-checked', 'true');
   await page.getByTestId('poi-edit-coordinates-textarea').click();
-  await page.waitForTimeout(2000);
   await page.getByTestId('poi-edit-coordinates-textarea').fill(formatted);
   await page.getByTestId('poi-edit-coordinates-textarea').press('Tab');
   await page.waitForTimeout(2000);
@@ -124,8 +127,9 @@ test('POI Management', async ({ page }) => {
 
   const EditPOI = page.locator('[role="alert"], .alert, .rz-notification').first(); // ensure single element
   const EditPOImessage = (await EditPOI.textContent())?.replace(/\s+/g, ' ').trim();
+  })
 
-  //Delete POI Group with Replacement
+  test('Delete POI Group with Replacement', async ({ page }) => {
   const deletegroup = page.getByRole('option', { name: Updategroup.title });
   await deletegroup.locator('button').click();
 
@@ -139,45 +143,38 @@ test('POI Management', async ({ page }) => {
 
   const DeletePOIGroup = page.locator('[role="alert"], .alert, .rz-notification').first(); // ensure single element
   const DeletePOIGroupmessage = (await DeletePOIGroup.textContent())?.replace(/\s+/g, ' ').trim();
-
-  console.table([
-    {
-      Action: 'Add POI Group',
-      Title: Addpoigroup.title,
-      Status: 'Passed',
-      Message: AddPOIGroupmessage
-    },
-    {
-      Action: 'Edit POI Group',
-      Title: Updategroup.title,
-      Status: 'Passed',
-      Message: EditPOIGroupmessage,
-    },
-    {
-      Action: 'Add POI',
-      Title: addpoi.name,
-      Status: 'Passed',
-      Message: AddPOImessage
-    },
-    {
-      Action: 'Edit POI',
-      Title: Updatepoi.name,
-      Status: 'Passed',
-      Message: EditPOImessage
-    },
-    {
-      Action: 'Delete POI Group',
-      Title: Updatepoi.name,
-      Status: 'Passed',
-      Message: DeletePOIGroupmessage
-    }
-  ]);
-
-  //Delete POI
-  // await page.getByRole('textbox', { name: 'Search...' }).click();
-  // await page.getByRole('textbox', { name: 'Search...' }).press('ControlOrMeta+a');
-  // await page.getByRole('textbox', { name: 'Search...' }).fill(Updatepoi.name);
-  // await page.getByRole('button', { name: 'delete' }).click();
-  // await expect(page.getByRole('dialog', { name: 'Confirm deletion' })).toBeVisible();
-  // await page.getByRole('button', { name: 'Delete', exact: true }).click();
+  })
 })
+//   console.table([
+//     {
+//       Action: 'Add POI Group',
+//       Title: Addpoigroup.title,
+//       Status: 'Passed',
+//       Message: AddPOIGroupmessage
+//     },
+//     {
+//       Action: 'Edit POI Group',
+//       Title: Updategroup.title,
+//       Status: 'Passed',
+//       Message: EditPOIGroupmessage,
+//     },
+//     {
+//       Action: 'Add POI',
+//       Title: addpoi.name,
+//       Status: 'Passed',
+//       Message: AddPOImessage
+//     },
+//     {
+//       Action: 'Edit POI',
+//       Title: Updatepoi.name,
+//       Status: 'Passed',
+//       Message: EditPOImessage
+//     },
+//     {
+//       Action: 'Delete POI Group',
+//       Title: Updatepoi.name,
+//       Status: 'Passed',
+//       Message: DeletePOIGroupmessage
+//     }
+//   ]);
+// })
